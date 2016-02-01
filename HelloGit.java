df965b586c9bc4a0db509119b80616a95bb13185
Tundrick <dkharebov@gmail.com>
2016-02-01 09:02:56 -0500

    Hello Git! commit.

---
+++ HelloGit.java
@@ -0,0 +1,12 @@
+
+public class HelloGit {
+	final static String strHello = "Hello Git!\n";
+
+	public static void main(String[] args) {
+		System.out.print(
+			""
+			+ strHello
+			+ "\n"
+			);
+	}
+}
